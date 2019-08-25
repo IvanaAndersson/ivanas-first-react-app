eaae6ef2d510d9de491366b345685c6c8527135d
Ivana Andersson <ivana.p.kostova@gmail.com>
2019-08-25 12:02:38 +0300

    add delete option on list item

--- src/components/FunctionalToDoList.jsx
+++ src/components/FunctionalToDoList.jsx
@@ -3,7 +3,6 @@ import React from "react";
 const FunctionalToDoList = props => {
   const [tasks, setTasks] = React.useState([]);
   const [input, setInput] = React.useState("");
-  React.useEffect();
 
   const handleChange = event => {
     setInput(event.target.value);
@@ -14,16 +13,32 @@ const FunctionalToDoList = props => {
     setInput("");
   };
 
+  const deleteTask = event => {
+    const index = event.target.dataset.index;
+    setTasks(tasks => {
+      const deleteTasks = [...tasks];
+      deleteTasks.splice(index, 1);
+      return deleteTasks;
+    });
+  };
+
   return (
     <div>
+      <h1>Current tasks:</h1>
       <ul>
         {tasks.map((task, i) => (
-          <li key={i}>{task}</li>
+          <li key={i}>
+            <button data-index={i} onClick={deleteTask}>
+              Delete
+            </button>
+            {task}
+          </li>
         ))}
       </ul>
       <div>
         <input onChange={handleChange} value={input} />
         <button onClick={addTask}>Add task</button>
+        <p>Number of tasks: {tasks.length}</p>
       </div>
     </div>
   );
